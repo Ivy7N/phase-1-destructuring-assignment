1a757ffa9af00b164c1a7c9b9c97020b744644ae
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-10-02 10:47:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, , g, b, v] = colors;
+const [,,,,indg] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
